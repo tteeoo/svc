6851c2288e5233e33f77064a163f9ab789727521
Theo Henson <theodorehenson@protonmail.com>
2020-12-15 19:32:47 -0500

    Implement more ops

--- cpu/cpu.go
+++ cpu/cpu.go
@@ -8,10 +8,11 @@ import (
 )
 
 const (
-	GPRNum = 8
-	PCNum = iota
+	GPRNum = 4
+	EXNum = iota
 	ACNum
 	SPNum
+	PCNum
 )
 
 // CPU is a basic implementation of a CPU.
@@ -33,19 +34,22 @@ func NewCPU() *CPU {
 	for i := range [GPRNum]uint16{} {
 		regs[uint16(i)] = NewRegister()
 	}
-	// Program counter
-	regs[GPRNum+PCNum] = NewRegister()
+	// Extra register
+	regs[GPRNum+EXNum] = NewRegister()
 	// Accumulator
 	regs[GPRNum+ACNum] = NewRegister()
 	// Stack pointer
 	regs[GPRNum+SPNum] = NewRegister()
+	// Program counter
+	regs[GPRNum+PCNum] = NewRegister()
 	return &CPU{
 		Mem:  mem.NewRAM(),
 		Regs: regs,
 		RegNames: map[string]uint16{
-			"pc": GPRNum+PCNum,
+			"ex": GPRNum+EXNum,
 			"ac": GPRNum+ACNum,
 			"sp": GPRNum+SPNum,
+			"pc": GPRNum+PCNum,
 		},
 		OpNames: map[string]uint16{
 			"nop": 0x00,
@@ -53,6 +57,17 @@ func NewCPU() *CPU {
 			"cpl": 0x02,
 			"str": 0x03,
 			"ldr": 0x04,
+			"add": 0x05,
+			"sub": 0x06,
+			"twc": 0x07,
+			"inc": 0x08,
+			"dec": 0x09,
+			"mul": 0x0a,
+			"div": 0x0b,
+			"dvc": 0x0c,
+			"xor": 0x0d,
+			"and": 0x0e,
+			"orr": 0x0f,
 		},
 	}
 }
@@ -74,8 +89,7 @@ func (c *CPU) GetOp(name string) uint16 {
 
 // Op executes an opcode with the given operands.
 func (c *CPU) Op(opcode uint16, operands []uint16) error {
-	fmt.Println("-->", opcode, operands)
-	fmt.Println(c)
+	fmt.Printf("--> %x, %x\n", opcode, operands)
 	switch opcode {
 	// nop
 	case 0x00:
@@ -100,7 +114,82 @@ func (c *CPU) Op(opcode uint16, operands []uint16) error {
 				c.Regs[operands[1]].Get(),
 			),
 		)
+	// add (reg with value)
+	case 0x05:
+		c.Regs[c.RegNames["ac"]].Set(
+			c.Regs[c.RegNames["ac"]].Get() + c.Regs[operands[0]].Get(),
+		)
+	// sub (reg with value)
+	case 0x06:
+		c.Regs[c.RegNames["ac"]].Set(
+			c.Regs[c.RegNames["ac"]].Get() + (^c.Regs[operands[0]].Get()+1),
+		)
+	// twc (reg to twc)
+	case 0x07:
+		c.Regs[operands[0]].Set(
+			^c.Regs[operands[0]].Get()+1,
+		)
+	// inc (reg to inc)
+	case 0x08:
+		c.Regs[operands[0]].Set(
+			c.Regs[operands[0]].Get() + 1,
+		)
+	// dec (reg to dec)
+	case 0x09:
+		c.Regs[operands[0]].Set(
+			c.Regs[operands[0]].Get() + (^uint16(1)+1),
+		)
+	// mul (reg with value)
+	case 0x0a:
+		c.Regs[c.RegNames["ac"]].Set(
+			c.Regs[c.RegNames["ac"]].Get() * c.Regs[operands[0]].Get(),
+		)
+	// div (reg with value)
+	case 0x0b:
+		c.Regs[c.RegNames["ex"]].Set(
+			c.Regs[c.RegNames["ac"]].Get() % c.Regs[operands[0]].Get(),
+		)
+		c.Regs[c.RegNames["ac"]].Set(
+			c.Regs[c.RegNames["ac"]].Get() / c.Regs[operands[0]].Get(),
+		)
+	// dvc (reg with value)
+	case 0x0c:
+		a := c.Regs[c.RegNames["ac"]].Get()
+		b := c.Regs[operands[0]].Get()
+		x := a
+		y := b
+		aSign := a >> 15
+		bSign := b >> 15
+		same := aSign == bSign
+		if aSign == 1 {
+			x = ^x+1
+		}
+		if bSign == 1 {
+			y = ^y+1
+		}
+		c.Regs[c.RegNames["ex"]].Set(x % y)
+		if same {
+			c.Regs[c.RegNames["ac"]].Set(x / y)
+		} else {
+			c.Regs[c.RegNames["ac"]].Set(^(x / y)+1)
+		}
+	// xor (reg with value)
+	case 0x0d:
+		c.Regs[c.RegNames["ac"]].Set(
+			c.Regs[c.RegNames["ac"]].Get() ^ c.Regs[operands[0]].Get(),
+		)
+	// and (reg with value)
+	case 0x0e:
+		c.Regs[c.RegNames["ac"]].Set(
+			c.Regs[c.RegNames["ac"]].Get() & c.Regs[operands[0]].Get(),
+		)
+	// orr (reg with value)
+	case 0x0f:
+		c.Regs[c.RegNames["ac"]].Set(
+			c.Regs[c.RegNames["ac"]].Get() | c.Regs[operands[0]].Get(),
+		)
 	}
+	fmt.Println(c)
 	return nil
 }
 
@@ -110,9 +199,10 @@ func (c *CPU) String() string {
 	for i := range [GPRNum]uint16{} {
 		out += fmt.Sprintf("\n%d:%s", i, c.Regs[uint16(i)])
 	}
-	out += fmt.Sprintf("\npc:%s", c.Regs[GPRNum+PCNum])
-	out += fmt.Sprintf("\nac:%s", c.Regs[GPRNum+ACNum])
-	out += fmt.Sprintf("\nsp:%s", c.Regs[GPRNum+SPNum])
+	out += fmt.Sprintf("\nex:%s", c.Regs[c.RegNames["ex"]])
+	out += fmt.Sprintf("\nac:%s", c.Regs[c.RegNames["ac"]])
+	out += fmt.Sprintf("\nsp:%s", c.Regs[c.RegNames["sp"]])
+	out += fmt.Sprintf("\npc:%s", c.Regs[c.RegNames["pc"]])
 	out += "\nMemory:\n"
 	out += c.Mem.String()
 	return out
--- main.go
+++ main.go
@@ -1,18 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"github.com/tteeoo/svc/cpu"
 )
 
 func main() {
 	c := cpu.NewCPU()
 	c.Op(c.GetOp("nop"), []uint16{})
-	c.Op(c.GetOp("cpl"), []uint16{0, 0xff})
-	c.Op(c.GetOp("cop"), []uint16{1, 0})
-	c.Op(c.GetOp("str"), []uint16{2, 1})
-	c.Op(c.GetOp("ldr"), []uint16{3, 2})
-	c.Op(c.GetOp("cpl"), []uint16{6, 0x41})
-	c.Op(c.GetOp("str"), []uint16{3, 6})
-	fmt.Println(c.GetMem())
+	c.Op(c.GetOp("cpl"), []uint16{c.RegNames["ac"], 4})
+	c.Op(c.GetOp("cpl"), []uint16{0, 4})
+	c.Op(c.GetOp("add"), []uint16{0})
 }
