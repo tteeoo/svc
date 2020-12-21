37c9ed62ab7371dd76604aedd26336272b6de8cc
Theo Henson <theodorehenson@protonmail.com>
2020-12-20 21:48:54 -0500

    Fix a.asm

--- asm/a.asm
+++ asm/a.asm
@@ -1,4 +1,5 @@
 main:
   cpl 0 0x0f41 ; copies 0x0f41 into register 0
-  str 0 0      ; stores the value held in register 0 at the first address in memory
+  cpl 1 0      ; copies 0 into register 1
+  str 1 0      ; stores the value held in register 0 at the first address in memory
   vga          ; draws the text buffer, printing "A" with white text and black background
